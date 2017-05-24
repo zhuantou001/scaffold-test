84dc85a456dbc120bd87e25f605c3bdfe06a2f0a
liuxiaojuan <332719886@qq.com>
2017-05-24 17:29:43 +0800

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <router-link to="/scaffold">Go to SCAFFOLD</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to SCAFFOLD'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
